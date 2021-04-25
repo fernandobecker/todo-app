957ed8582343e282e9bc19961b7ee444425e753e
Fernando <fernando.silva@agriness.com>
2021-04-25 12:37:30 -0300

    Add items todo

--- src/to-do.js
+++ src/to-do.js
@@ -1,16 +1,48 @@
 import React from 'react';
 
 class TODO extends React.Component{
+    
     constructor(props){
         super(props);
+        this.state = {
+            text: "",
+            items: []
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleChange(e) {
+        this.setState({text: e.target.value})
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        if(this.state.text.lenght === 0) return;
+
+        const newItem = {
+            text: this.state.text,
+            id: Date.now()
+        }
+
+        this.setState(state => ({
+            items: state.items.concat(newItem),
+            text: ''
+        }))
+    }
+    
     render(){
         return (
             <div>
                 <h1>TODO Application</h1>
-                <form>
+                <ul>
+                    {this.state.items.map(item => (
+                        <li key={item.id}>{item.value}</li>
+                    ))}
+                </ul>
+                <form onSubmit={this.handleSubmit}>
                     <label htmlfor="id">Input Item</label>
-                    <input id="id"/>
+                    <input id="id" onChange={this.handleChange} value={this.state.text}/>
                     <button>Add</button>
                 </form>
             </div>
